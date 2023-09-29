887bbca28396047de843a724561e1515a327b2ae
islu <38793955+islu@users.noreply.github.com>
2023-09-29 17:23:57 +0800

    move func

--- bard.go
+++ bard.go
@@ -10,10 +10,35 @@ import (
 	"net/http"
 	"net/http/cookiejar"
 	"net/url"
+	"os"
 	"regexp"
 	"strings"
 	"time"
+
+	_ "github.com/joho/godotenv/autoload"
 )
+
+func NewChat() {
+	bardApiKey := os.Getenv("BARD_API_KEY")
+
+	bot, err := NewChatbot(bardApiKey)
+	if err != nil {
+		fmt.Println("new error")
+		log.Fatalln(err)
+		return
+	}
+
+	resp, err := bot.Ask("明天是中秋節")
+	if err != nil {
+		fmt.Println("ask error")
+		log.Fatalln(err)
+		return
+	}
+
+	fmt.Println(resp.ResponseID)
+	fmt.Println(resp.ConversationID)
+	fmt.Println(resp.Content)
+}
 
 type Chatbot struct {
 	ReqID          int
--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-
-	_ "github.com/joho/godotenv/autoload"
-)
-
 func main() {
-	bardApiKey := os.Getenv("BARD_API_KEY")
 
-	bot, err := NewChatbot(bardApiKey)
-	if err != nil {
-		fmt.Println("new error")
-		log.Fatalln(err)
-		return
-	}
-
-	resp, err := bot.Ask("明天是中秋節")
-	if err != nil {
-		fmt.Println("ask error")
-		log.Fatalln(err)
-		return
-	}
-
-	fmt.Println(resp.ResponseID)
-	fmt.Println(resp.ConversationID)
-	fmt.Println(resp.Content)
 }
