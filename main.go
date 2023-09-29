7dcbe4d7964eeb1926575b65a33e6624473dd134
islu <38793955+islu@users.noreply.github.com>
2023-09-29 17:43:48 +0800

    add ebiten

--- main.go
+++ main.go
@@ -1,5 +1,36 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+)
+
+const (
+	ScreenWidth  = 320
+	ScreenHeight = 240
+)
+
+type Game struct {
+}
+
+func (g *Game) Update() error {
+	return nil
+}
+
+func (g *Game) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrint(screen, "Hello World")
+}
+
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return ScreenWidth, ScreenHeight
+}
+
+func main() {
+	ebiten.SetWindowSize(ScreenWidth*2, ScreenHeight*2)
+	ebiten.SetWindowTitle("Hello World")
+	if err := ebiten.RunGame(&Game{}); err != nil {
+		log.Fatal(err)
+	}
 }
