0de41038319df9b8c3f1b2bbc330931a06cdebbf
islu <38793955+islu@users.noreply.github.com>
2023-09-30 22:40:03 +0800

    draw img

--- main.go
+++ main.go
@@ -2,17 +2,21 @@ package main
 
 import (
 	"log"
+	"os"
+
+	"image"
+	_ "image/png"
 
 	"github.com/hajimehoshi/ebiten/v2"
-	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 )
 
 const (
-	ScreenWidth  = 320
-	ScreenHeight = 240
+	screenWidth  = 320
+	screenHeight = 240
 )
 
 type Game struct {
+	bard *ebiten.Image
 }
 
 func (g *Game) Update() error {
@@ -20,17 +24,39 @@ func (g *Game) Update() error {
 }
 
 func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello World")
+	op := &ebiten.DrawImageOptions{}
+	op.GeoM.Scale(0.05, 0.05)
+	op.GeoM.Translate(0, 64)
+	screen.DrawImage(g.bard, op)
 }
 
 func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return ScreenWidth, ScreenHeight
+	return screenWidth, screenHeight
 }
 
 func main() {
-	ebiten.SetWindowSize(ScreenWidth*2, ScreenHeight*2)
-	ebiten.SetWindowTitle("Hello World")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	f, err := os.Open("bard.png")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	img, _, err := image.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bard := ebiten.NewImageFromImage(img)
+
+	g := &Game{
+		bard: bard,
+	}
+
+	ebiten.SetWindowSize(screenWidth*2, screenHeight*2)
+	ebiten.SetWindowTitle("Eccl.7.8")
+	// ebiten.SetWindowDecorated(false)
+	ebiten.SetWindowFloating(true)
+	if err := ebiten.RunGame(g); err != nil {
 		log.Fatal(err)
 	}
 }
